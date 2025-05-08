cb6c07dc1dd3802712343b410376cdc7547c15c7
berketufan000 <berketufan00@gmail.com>
2025-05-08 22:52:14 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
-@echo off
-echo Starting the script...
-pip install -r requirements.txt > NUL 2>&1
+echo "Starting the script..."
+pip install -r requirements.txt > /dev/null 2>&1
 python script.py
-echo Script finished.
-pause
+echo "Script finished."
